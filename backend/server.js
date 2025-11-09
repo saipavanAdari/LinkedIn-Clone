a6b7f2cc44c02f253d3cdeebac54b6ac3fb0c0de
saipavanAdari <adarisaipavan0504@gmail.com>
2025-11-09 19:50:52 +0530

    frontend url for backend use

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: "https://linkedin-clone-d2f7.onrender.com",
     credentials: true,
   })
 );
