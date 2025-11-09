4c3f20939f133a2169cd8e1fb0a9b033373b0682
saipavanAdari <adarisaipavan0504@gmail.com>
2025-11-09 21:18:36 +0530

    backend url correction

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    // origin: "http://localhost:3000",
     origin: "https://linkedin-clone-d2f7.onrender.com",
     credentials: true,
   })
